e4ef8ea41fbfe967d1986900b00ed0661283796a
jessicasiburian <jessicasiburian969@gmail.com>
2020-06-23 18:08:29 +0700

    database yang baru

--- PBO/Web Service/AppKomplainBarangRusak/src/routes.php
+++ PBO/Web Service/AppKomplainBarangRusak/src/routes.php
@@ -23,4 +23,57 @@ return function (App $app) {
         return $response->withJson(["status" => "success", "data" => $result], 200);
     });
 
+    $app->post("/keluhan/create/", function(Request $request, Response $response){
+        $data = $request->getParsedBody();
+        $id_pengguna = $data['id_pengguna'];
+        $nama_lengkap = $data['nama_lengkap'];
+        $nama_toko = $data['nama_toko'];
+        $nama_barang = $data['nama_barang'];
+        $tipe_barang = $data['tipe_barang'];
+        $tgl_order = $data['tgl_order'];
+        $keluhan = $data['keluhan'];
+        $sql = "INSERT INTO komplain values(:id_pengguna, :nama_lengkap, :nama_toko, :nama_barang, :tipe_barang, :tgl_order, :keluhan)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam("id_pengguna", $id_pengguna);
+        $stmt->bindParam("nama_lengkap", $nama_lengkap);
+        $stmt->bindParam("nama_toko", $nama_toko);
+        $stmt->bindParam("nama_barang", $nama_barang);
+        $stmt->bindParam("tipe_barang", $tipe_barang);
+        $stmt->bindParam("tgl_order", $tgl_order);
+        $stmt->bindParam("keluhan", $keluhan);
+        $result = $stmt->execute();
+        if(sizeof($result)==0){//apabila data peserta tidak ditemukan
+            return $response->withJson(["status" => "error", "message" => "Data tidak berhasil dimasukkan"], 200);
+        }
+        else {
+            return $response->withJson(["status" => "success", "data" => "Data berhasil dimasukkan"], 200);
+        }
+    });
+
+        $app->post("/keluhan/create/", function(Request $request, Response $response){
+        $data = $request->getParsedBody();
+        $id_pengguna = $data['id_pengguna'];
+        $nama_lengkap = $data['nama_lengkap'];
+        $nama_toko = $data['nama_toko'];
+        $nama_barang = $data['nama_barang'];
+        $tipe_barang = $data['tipe_barang'];
+        $tgl_order = $data['tgl_order'];
+        $keluhan = $data['keluhan'];
+        $sql = "UPDATE komplain SET (:id_pengguna, :nama_lengkap, :nama_toko, :nama_barang, :tipe_barang, :tgl_order, :keluhan)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam("id_pengguna", $id_pengguna);
+        $stmt->bindParam("nama_lengkap", $nama_lengkap);
+        $stmt->bindParam("nama_toko", $nama_toko);
+        $stmt->bindParam("nama_barang", $nama_barang);
+        $stmt->bindParam("tipe_barang", $tipe_barang);
+        $stmt->bindParam("tgl_order", $tgl_order);
+        $stmt->bindParam("keluhan", $keluhan);
+        $result = $stmt->execute();
+        if(sizeof($result)==0){//apabila data peserta tidak ditemukan
+            return $response->withJson(["status" => "error", "message" => "Data tidak berhasil dimasukkan"], 200);
+        }
+        else {
+            return $response->withJson(["status" => "success", "data" => "Data berhasil dimasukkan"], 200);
+        }
+    });
 };
