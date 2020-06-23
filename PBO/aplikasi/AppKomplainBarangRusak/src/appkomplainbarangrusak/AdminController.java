a3bc99b4763f009e94c479b958f49a2f4ffba373
jessicasiburian <jessicasiburian969@gmail.com>
2020-06-23 09:52:43 +0700

    admin

---
+++ PBO/aplikasi/AppKomplainBarangRusak/src/appkomplainbarangrusak/AdminController.java
@@ -0,0 +1,15 @@
+package appkomplainbarangrusak;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class AdminController implements Initializable {
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
